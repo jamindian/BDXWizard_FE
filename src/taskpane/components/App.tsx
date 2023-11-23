ba3b129a71d320e9a59456fe7bd8eb65e4a7bf9c
Hafiz suleman <52320313+hafiz422231@users.noreply.github.com>
2023-11-23 20:04:47 +0500

    isLoaderSelector added

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -3,6 +3,8 @@ import * as React from "react";
 import { makeStyles } from "@fluentui/react-components";
 import AppLoader from "./AppLoader";
 import DashboardButtons from "./DashboardButtons/DashboardButtons";
+import { useSelector } from "react-redux";
+import { isLoaderSelector } from "@redux/Actions/Auth";
 
 const useStyles = makeStyles({
   root: {
@@ -12,7 +14,7 @@ const useStyles = makeStyles({
 
 const App = () => {
   const styles = useStyles();
-  const [loader, setLoader] = React.useState<boolean>(false);
+  const loader: boolean = useSelector(isLoaderSelector);
 
   return (
     <div className={styles.root}>
